e986cbb2ca488f01fa92f9c943c558af8b8cd614
Sean Farley <sean@mcs.anl.gov>
2012-01-25 10:39:41 -0600

    bash: add scripts to wrap ssh and emacs commands so that remote editing works
    This required new .hgignores (for the tramp and shell commands),
    updating the 'e' alias to point to the ~/.emacs.sh wrapper. The emacs
    wrapper needed to be a script, not a bash function, so that mercurial
    et. al could call it.

---
+++ .emacs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+ED="emacs"
+FN="$1"
+SF="$HOME/.emacs.d/server/server"
+PORT="1"
+HN=$(hostname)
+
+# if server/server file exists, then grep it for the port
+[[ -r $SF ]] && PORT=$(egrep -o '127.0.0.1:([0-9]*)' $SF | sed 's/127.0.0.1://')
+
+# check to see if port is open; apparently, ubuntu needs the -v flag
+[[ -n $(nc -zv 127.0.0.1 $PORT 2>&1 | grep succeeded) || $HN == *seanfarley* ]] && ED="emacsclient -f $SF"
+
+# build the tramp filename or local filename
+[[ $HN != *seanfarley* ]] && FN="/$(whoami)@$HN:$1"
+
+$ED $FN
