2a5907562914496cbcbfdea2e78ef0c78093860a
Sean Farley <sean@mcs.anl.gov>
2012-01-25 15:05:33 -0600

    bash: fix emacs.sh globbing to match local hostname better

--- .emacs.sh
+++ .emacs.sh
@@ -10,9 +10,11 @@ HN=$(hostname)
 [[ -r $SF ]] && PORT=$(egrep -o '127.0.0.1:([0-9]*)' $SF | sed 's/127.0.0.1://')
 
 # check to see if port is open; apparently, ubuntu needs the -v flag
-[[ -n $(nc -zv 127.0.0.1 $PORT 2>&1 | grep succeeded) || $HN == *seanfarley* ]] && ED="emacsclient -f $SF"
+[[ -n $(nc -zv 127.0.0.1 $PORT 2>&1 | grep succeeded) || $HN == seanfarley* ]] && ED="emacsclient -f $SF"
 
-# build the tramp filename or local filename
-[[ $HN != *seanfarley* ]] && FN="/$(whoami)@$HN:$1"
+# build the tramp filename or local filename also, I'm assuming any
+# hostname that starts with 'seanfarley' is my local
+# (e.g. seanfarley.local, seanfarley.mcs.anl.gov)
+[[ $HN != seanfarley* ]] && FN="/$(whoami)@$HN:$1"
 
 $ED $FN
