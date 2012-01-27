0736a74505bab866ffde2f0f7ac6070e82b7b5ab
Sean Farley <sean@mcs.anl.gov>
2012-01-27 11:32:44 -0600

    bash: added option parsing to emacs.sh

--- .emacs.sh
+++ .emacs.sh
@@ -1,10 +1,36 @@
 #!/usr/bin/env bash
 
 ED="emacs"
-FN=$(readlink -f $1)
+OPTS=""
 SF="$HOME/.emacs.d/server/server"
 PORT="1"
 HN=$(hostname)
+
+# dev note: since we're using bash 'getopts' arguments MUST be before the filename provided
+while getopts ":h" opt; do
+  case $opt in
+    h)
+    cat << EOF
+usage: $0 options
+
+This script calls emacsclient if the file $SF exists and the reverse port is open, else if it calls emacs
+
+OPTIONS:
+  -h      Show this message
+  any other option will be passed to emacs or emacsclient
+EOF
+    exit 0
+    ;;
+    \?)
+    OPTS="$OPTS -$OPTARG"
+    ;;
+  esac
+done
+
+# shift the optional flags provided before the filename out of the way
+shift $(( OPTIND-1 ))
+
+FN=$(readlink -f $1)
 
 # if $FN is empty then this usually means a non-existent directory was provided
 [[ -z $FN ]] && echo "error: cannot create directories (check the file path provided)" && exit 1
@@ -20,4 +46,4 @@ HN=$(hostname)
 # (e.g. seanfarley.local, seanfarley.mcs.anl.gov)
 [[ $HN != seanfarley* ]] && FN="/$(whoami)@$HN:$FN"
 
-$ECHO $ED $FN
+$ECHO $ED $OPTS $FN
