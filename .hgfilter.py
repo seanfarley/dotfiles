710c10cc48460b7d4848a33e6ee4fc2124bd9310
Sean Farley <sean@mcs.anl.gov>
2012-02-08 16:50:42 -0600

    hg: changed fill76 to fill180

--- .hgfilter.py
+++ .hgfilter.py
@@ -3,4 +3,5 @@ from mercurial import templatefilters
 # for now this is just for phases
 def extsetup(ui):
     templatefilters.filters["permute"] = lambda x: ([3,2,1][x])
+    templatefilters.filters["fill180"] = lambda x: templatefilters.fill(x,180)
 
