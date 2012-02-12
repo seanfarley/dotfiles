df563bc6a8c183fda699c103dcb7da451b829407
Sean Farley <sean@mcs.anl.gov>
2012-02-12 17:02:05 -0600

    bash: fix emacs.sh to work with mercurial merging

--- .emacs.sh
+++ .emacs.sh
@@ -6,11 +6,14 @@ SF="$HOME/.emacs.d/server/server"
 PORT="1"
 HN=$(hostname)
 
-# dev note: since we're using bash 'getopts' arguments MUST be before the filename provided
-while getopts ":h" opt; do
-  case $opt in
-    h)
-    cat << EOF
+# hack: if options contain --eval then don't parse args
+
+if [[ $@ != *--eval* ]]; then
+  # dev note: since we're using bash 'getopts' arguments MUST be before the filename provided
+  while getopts ":h" opt; do
+    case $opt in
+      h)
+        cat << EOF
 usage: $0 options
 
 This script calls emacsclient if the file $SF exists and the reverse port is open, else if it calls emacs
@@ -19,21 +22,24 @@ OPTIONS:
   -h      Show this message
   any other option will be passed to emacs or emacsclient
 EOF
-    exit 0
-    ;;
-    \?)
-    OPTS="$OPTS -$OPTARG"
-    ;;
-  esac
-done
+        exit 0
+        ;;
+      \?)
+        OPTS="$OPTS -$OPTARG"
+        ;;
+    esac
+  done
 
-# shift the optional flags provided before the filename out of the way
-shift $(( OPTIND-1 ))
+  # shift the optional flags provided before the filename out of the way
+  shift $(( OPTIND-1 ))
 
-FN=$(readlink -f $1)
+  FN=$(readlink -f $1 2>/dev/null)
+else
+  FN=$@
+fi
 
 # if $FN is empty then this usually means a non-existent directory was provided
-[[ -z $FN ]] && echo "error: cannot create directories (check the file path provided)" && exit 1
+[[ -z $FN ]] && echo "error: check FN='$FN'" && exit 1
 
 # if server/server file exists, then grep it for the port
 [[ -r $SF ]] && PORT=$(egrep -o '127.0.0.1:([0-9]*)' $SF | sed 's/127.0.0.1://')
@@ -46,4 +52,4 @@ FN=$(readlink -f $1)
 # (e.g. seanfarley.local, seanfarley.mcs.anl.gov)
 [[ $HN != seanfarley* ]] && FN="/$(whoami)@$HN:$FN"
 
-$ECHO $ED $OPTS $FN
+$ECHO eval "$ED $OPTS $FN"
