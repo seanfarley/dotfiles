4b4c60fa2730edf753b82a53c03157fdef48fafe
Sean Farley <sean@mcs.anl.gov>
2012-04-30 10:11:00 -0500

    hg: changed colors for phases; back to red for secret

--- .hgfilter.py
+++ .hgfilter.py
@@ -2,6 +2,6 @@ from mercurial import templatefilters
 
 # for now this is just for phases
 def extsetup(ui):
-    templatefilters.filters["phasecolor"] = lambda x: (['1;31m','0;32m','0;34m'][x])
+    templatefilters.filters["phasecolor"] = lambda x: (['0;34m','0;32m','1;31m'][x])
     templatefilters.filters["fill180"] = lambda x: templatefilters.fill(x,180)
 
