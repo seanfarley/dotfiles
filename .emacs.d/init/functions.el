482a5b9c8855ffb318a6d09bb5580ded2bc24dfa
Sean Farley <sean@mcs.anl.gov>
2012-05-18 22:39:42 -0500

    emacs: add sudo-edit command that actually works

--- .emacs.d/init/functions.el
+++ .emacs.d/init/functions.el
@@ -72,3 +72,8 @@
 
 (add-hook 'post-command-hook 'djcb-set-cursor-according-to-mode)
 
+(defun sudo-edit (&optional arg)
+  (interactive "p")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:" (ido-read-file-name "File: ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
