6d449db0f6c566f20bafcbee36b3387a3dddd80b
Sean Farley <sean@mcs.anl.gov>
2012-08-15 19:17:12 -0500

    mercurial: use builtin fill76 instead of custom filter

--- .hgfilter.py
+++ .hgfilter.py
@@ -3,5 +3,4 @@ from mercurial import templatefilters
 # for now this is just for phases
 def extsetup(ui):
     templatefilters.filters["phasecolor"] = lambda x: (['0;34m','0;32m','1;31m'][x])
-    templatefilters.filters["fill180"] = lambda x: templatefilters.fill(x,180)
 
