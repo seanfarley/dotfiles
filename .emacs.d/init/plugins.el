491855d3e5146ca340fbc266b8a81e54d36c12f2
Sean Farley <sean@mcs.anl.gov>
2012-09-16 21:38:14 -0500

    emacs: add pymacs

--- .emacs.d/init/plugins.el
+++ .emacs.d/init/plugins.el
@@ -163,3 +163,11 @@
 
 ; python.el
 (require 'python)
+
+; pymacs
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(autoload 'pymacs-autoload "pymacs")
