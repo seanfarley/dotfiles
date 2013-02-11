c8eb0e8d854435d87f9b4bf25c05fbfc9dc9206a
Sean Farley <sean.michael.farley@gmail.com>
2013-02-10 19:07:16 -0600

    emacs: clean up comments for keybindings

--- .emacs.d/init/keybindings.el
+++ .emacs.d/init/keybindings.el
@@ -4,14 +4,14 @@
 
 ; Add modifier keys for M-x
 
-; Moving between windows
+;; Moving between windows
 (global-set-key (kbd "C-H-<up>") 'windmove-up)
 (global-set-key (kbd "C-H-<down>") 'windmove-down)
 (global-set-key (kbd "C-H-<right>") 'windmove-right)
 (global-set-key (kbd "C-H-<left>") 'windmove-left)
 
-; Cmd-backspace delete to beginning of line (by default, you can do
-; this is emacs with C-0 C-k)
+;; cmd-backspace delete to beginning of line (by default, you can do
+;; this is emacs with C-0 C-k)
 (global-set-key [s-backspace] '(lambda () (interactive) (kill-line 0)))
 
 (global-set-key (kbd "C-H-f") 'toggle-fullscreen)
