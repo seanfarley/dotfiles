465596335bf085daa234e6a3c68957c25fc60ed9
Sean Farley <sean.michael.farley@gmail.com>
2013-06-28 12:19:56 -0500

    emacs: add web-mode

--- .emacs.d/init/plugins.el
+++ .emacs.d/init/plugins.el
@@ -203,3 +203,14 @@
 (when (require 'w3m-load nil 'noerror)
   (setq w3m-display-inline-images t)
 )
+
+; web mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
