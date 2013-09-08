25ba8ab903422ddc2a181a4f9069295779a94901
Sean Farley <sean.michael.farley@gmail.com>
2013-09-08 15:32:49 -0500

    bash: add fallback path to emacs.sh

--- .emacs.sh
+++ .emacs.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 ED="emacs"
+EC="emacsclient"
 OPTS=""
 SF="$HOME/.emacs.d/server/server"
 PORT="1"
 HN=$(hostname -f)
+
+# check for emacsclient path, fallback to home directory
+hash emacsclient 2>/dev/null || EC="$HOME/.local/bin/emacsclient"
 
 # hack: if on euclid, change to a public name
 [[ $HN == euclid* ]] && HN=${HN/euclid/smf}
@@ -48,7 +52,7 @@ fi
 [[ -r $SF ]] && PORT=$(egrep -o '127.0.0.1:([0-9]*)' $SF | sed 's/127.0.0.1://')
 
 # check to see if port is open; apparently, ubuntu needs the -v flag
-[[ -n $(nc -zv 127.0.0.1 $PORT 2>&1 | grep 'succeeded\|open') || $HOME == /Users/$USER ]] && ED="emacsclient -f $SF"
+[[ -n $(nc -zv 127.0.0.1 $PORT 2>&1 | grep 'succeeded\|open') || $HOME == /Users/$USER ]] && ED="$EC -f $SF"
 
 # build the tramp filename or local filename also
 [[ $HOME != /Users/$USER ]] && FN="/$(whoami)@$HN:$FN"
