091ca2da79afe867a4f51d7777a96c36d7d58bd8
Sean Farley <sean.michael.farley@gmail.com>
2013-11-07 13:24:07 -0600

    emacs: remove custom texify function in favor of latexmk

--- emacs/init/tex.el
+++ emacs/init/tex.el
@@ -237,52 +237,5 @@
                                      TeX-command-Show))
           (TeX-command-Show)))
 
-  ;;;  from wiki
-  (defcustom TeX-texify-Show nil
-    "Start view-command at end of TeX-texify?"
-    :type 'boolean
-    :group 'TeX-command)
-
-  (defcustom TeX-texify-max-runs-same-command 5
-    "Maximal run number of the same command"
-    :type 'integer
-    :group 'TeX-command)
-
-  (defun TeX-texify-sentinel (&optional proc sentinel)
-    "Non-interactive! Call the standard-sentinel of the current LaTeX-process.
- If there is still something left do do start the next latex-command."
-    (set-buffer (process-buffer proc))
-    (funcall TeX-texify-sentinel proc sentinel)
-    (let ((case-fold-search nil))
-      (when (string-match "\\(finished\\|exited\\)" sentinel)
-        (set-buffer TeX-command-buffer)
-        (unless (plist-get TeX-error-report-switches (intern (TeX-master-file)))
-          (TeX-texify)))))
-
-  (defun TeX-texify ()
-    "Get everything done."
-    (interactive)
-    (let ((nextCmd (TeX-command-default (TeX-master-file)))
-          proc)
-      (if (and (null TeX-texify-Show)
-               (equal nextCmd TeX-command-Show))
-          (when  (called-interactively-p 'any)
-            (message "TeX-texify: Nothing to be done."))
-        (TeX-command nextCmd 'TeX-master-file)
-        (when (or (called-interactively-p 'any)
-                  (null (boundp 'TeX-texify-count-same-command))
-                  (null (boundp 'TeX-texify-last-command))
-                  (null (equal nextCmd TeX-texify-last-command)))
-          (mapc 'make-local-variable '(TeX-texify-sentinel TeX-texify-count-same-command TeX-texify-last-command))
-          (setq TeX-texify-count-same-command 1))
-        (if (>= TeX-texify-count-same-command TeX-texify-max-runs-same-command)
-            (message "TeX-texify: Did %S already %d times. Don't want to do it anymore." TeX-texify-last-command TeX-texify-count-same-command)
-          (setq TeX-texify-count-same-command (1+ TeX-texify-count-same-command))
-          (setq TeX-texify-last-command nextCmd)
-          (and (null (equal nextCmd TeX-command-Show))
-               (setq proc (get-buffer-process (current-buffer)))
-               (setq TeX-texify-sentinel (process-sentinel proc))
-               (set-process-sentinel proc 'TeX-texify-sentinel))))))
-
-  (add-hook 'LaTeX-mode-hook '(lambda () (define-key LaTeX-mode-map (kbd "C-c C-a") 'TeX-texify)))
+  (add-hook 'LaTeX-mode-hook '(lambda () (define-key LaTeX-mode-map (kbd "C-c C-a") '(lambda () (interactive) (TeX-command "LaTeX Make" 'TeX-master-file -1)))))
 )
