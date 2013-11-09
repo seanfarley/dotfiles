3816500a9172f90a3521d5eda85c84549469c560
Sean Farley <sean.michael.farley@gmail.com>
2013-11-08 20:20:24 -0600

    emacs: move ac-math to tex.el

--- emacs/init/plugins.el
+++ emacs/init/plugins.el
@@ -126,19 +126,6 @@
      (real-global-auto-complete-mode t)
      (ac-flyspell-workaround)))
 
-; ac-math
-(autoload 'ac-math "latex-mode" "Auto-complete Math Mode." t)
-(eval-after-load "ac-math"
-  '(progn
-     (add-to-list 'ac-modes 'latex-mode)   ; make auto-complete aware of {{{latex-mode}}}
-     (defun ac-latex-mode-setup ()         ; add ac-sources to default ac-sources
-       (setq ac-sources
-             (append '(ac-source-math-unicode
-                       ac-source-math-latex
-                       ac-source-latex-commands)
-                     ac-sources)))
-     (add-hook 'LaTeX-mode-hook 'ac-latex-mode-setup)))
-
 ; load codesearch
 (require 'codesearch)
 
--- emacs/init/tex.el
+++ emacs/init/tex.el
@@ -6,6 +6,17 @@
   (require 'auctex-latexmk)
   (auctex-latexmk-setup)
 
+  ; ac-math
+  (require 'ac-math) ; "LaTeX-mode" "Auto-complete Math Mode." t)
+  (add-to-list 'ac-modes 'latex-mode)   ; make auto-complete aware of {{{latex-mode}}}
+  (defun ac-latex-mode-setup ()         ; add ac-sources to default ac-sources
+    (setq ac-sources
+          (append '(ac-source-math-unicode
+                    ac-source-math-latex
+                    ac-source-latex-commands)
+                  ac-sources)))
+  (add-hook 'LaTeX-mode-hook 'ac-latex-mode-setup)
+
   (setq
    TeX-parse-self t
    TeX-auto-save t
