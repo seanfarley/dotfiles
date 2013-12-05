b7d0301fc99a19b13235a67b4f7c046e65832fe7
Sean Farley <sean.michael.farley@gmail.com>
2013-12-05 15:08:34 -0600

    emacs: remove old snippets directory

--- emacs/init/plugins.el
+++ emacs/init/plugins.el
@@ -155,8 +155,7 @@
 
 ; yasnippet
 (require 'yasnippet)
-(setq yas/root-directory '("~/.emacs.d/snippets"
-"~/.emacs.d/plugins/yasnippet/snippets"))
+(setq yas/root-directory '("~/.emacs.d/snippets"))
 (mapc 'yas/load-directory yas/root-directory)
 
 (yas-global-mode 1)
