5ce186dc5d6e755b00a40e2f94fdb14c80859e19
Sean Farley <sean.michael.farley@gmail.com>
2014-04-29 13:10:00 -0500

    emacs: update labels for mail.el

--- emacs/init/mail.el
+++ emacs/init/mail.el
@@ -3,7 +3,7 @@
 (eval-after-load "mu4e"
   '(progn
   ;; define my custom set of labels for easy word completion
-  (setq mylabels (list "\\Trash" "/Inbox" "Action" "R/anl" "R/aperture" "R/applications" "R/armins-wedding" "R/barryisms" "R/bout" "R/chipy" "R/documents" "R/facets" "R/family" "R/friends" "R/GSoC" "R/iit" "R/karlin" "R/karlin-users" "R/job" "R/llnl" "R/lsmsa" "R/lsmsa-elections" "R/lsmsa.net" "R/lsu" "R/macports" "R/math" "R/mercurial" "R/nersc" "R/petsc-announce" "R/petsc-dev" "R/petsc-exchange" "R/petsc-maint" "R/petsc-students" "R/petsc-users" "R/phd" "R/posterity" "R/recipes" "R/reunion" "R/server" "R/shelved-projects" "R/siam" "R/talks" "R/teaching" "R/tips" "R/V2" "R/vera" "R/website" "R/zfs"))
+  (setq mylabels (list "\\Trash" "\\Inbox" "\\Spam" "Action" "R/anl" "R/aperture" "R/applications" "R/armins-wedding" "R/barryisms" "R/bout" "R/chipy" "R/documents" "R/facets" "R/family" "R/friends" "R/GSoC" "R/iit" "R/karlin" "R/karlin-users" "R/job" "R/llnl" "R/lsmsa" "R/lsmsa-elections" "R/lsmsa.net" "R/lsu" "R/macports" "R/math" "R/mercurial" "R/nersc" "R/petsc-announce" "R/petsc-dev" "R/petsc-exchange" "R/petsc-maint" "R/petsc-students" "R/petsc-users" "R/phd" "R/posterity" "R/recipes" "R/reunion" "R/server" "R/shelved-projects" "R/siam" "R/talks" "R/teaching" "R/tips" "R/V2" "R/vera" "R/website" "R/zfs"))
 
   ;; use imagemagick, if available
   (when (fboundp 'imagemagick-register-types)
