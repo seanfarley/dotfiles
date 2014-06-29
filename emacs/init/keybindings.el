d2679c22717842003711a75d89bef6b875db0cb7
Sean Farley <sean.michael.farley@gmail.com>
2014-06-29 16:51:28 -0500

    emacs: remove old keybinding for deleting a line

--- emacs/init/keybindings.el
+++ emacs/init/keybindings.el
@@ -13,7 +13,6 @@
 
 ;; cmd-backspace delete to beginning of line (by default, you can do
 ;; this is emacs with C-0 C-k)
-(global-set-key [s-backspace] '(lambda () (interactive) (kill-line 0)))
 (global-set-key [H-backspace] [?\C-  ?\C-a backspace])
 
 (global-set-key (kbd "C-H-f") 'toggle-fullscreen)
