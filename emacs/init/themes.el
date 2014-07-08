9d55009a7d340516d903c23f8de865e0660d00ff
Sean Farley <sean.michael.farley@gmail.com>
2014-07-08 16:52:39 -0500

    emacs: rename solarized directory

--- emacs/init/themes.el
+++ emacs/init/themes.el
@@ -1,4 +1,4 @@
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/solarized-emacs")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/zenburn-emacs")
 
 ;(load-theme 'solarized-dark t)
