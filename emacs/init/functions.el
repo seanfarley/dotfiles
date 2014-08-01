c018edd73830999698c87329e705ae82c37b964f
Sean Farley <sean.michael.farley@gmail.com>
2014-08-01 13:16:02 -0500

    emacs: bury compile buffer if successful

--- emacs/init/functions.el
+++ emacs/init/functions.el
@@ -167,3 +167,19 @@ given, kill the region, adding it to the kill ring."
       (delete-region (point) (save-excursion (skip-syntax-backward " ") (point)))
       (delete-region (point) (save-excursion (skip-syntax-backward "^ ") (point))))))
 (global-set-key "\C-w" 'my-delete-backward-to-ws)
+
+;; auto bury the compilation buffer if successful
+(defun bury-compile-buffer-if-successful (buffer string)
+  "Bury a compilation buffer if succeeded without warnings "
+  (if (and
+       (string-match "compilation" (buffer-name buffer))
+       (string-match "finished" string)
+       (not
+        (with-current-buffer buffer
+          (search-forward "warning" nil t))))
+      (run-with-timer 1 nil
+                      (lambda (buf)
+                        (bury-buffer buf)
+                        (switch-to-prev-buffer (get-buffer-window buf) 'kill))
+                      buffer)))
+(add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful)
