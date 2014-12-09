ebd11f40e0be173d298d1a82562ab14d931b0b57
Sean Farley <sean.michael.farley@gmail.com>
2014-12-09 14:30:30 -0800

    emacs: remove old implementation

--- emacs/init/themes.el
+++
@@ -1,7 +0,0 @@
-(setq load-path (cons "~/.emacs.d/themes/solarized-emacs" load-path))
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/solarized-emacs")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/zenburn-emacs")
-
-;(load-theme 'solarized-dark t)
-;(load-theme 'solarized-light t)
-(load-theme 'zenburn t)
