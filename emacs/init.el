264f06db760e413028c28d9cc9648d64df2f5c62
Sean Farley <sean@farley.io>
2015-09-16 10:33:36 -0700

    emacs: add package-initialize

--- emacs/init.el
+++ emacs/init.el
@@ -4,6 +4,13 @@
 
 ;; This file uses org-babel to load my literate configuration.
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'ob-tangle)
 
 (org-babel-load-file (expand-file-name "README.org" (file-name-directory
