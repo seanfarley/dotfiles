2a9bb0585d71254d9e7ca6ebbad9623ba750cf77
Sean Farley <sean@farley.io>
2017-02-19 01:00:52 -0800

    emacs: remove last of require-package

--- emacs/init.el
+++ emacs/init.el
@@ -21,22 +21,6 @@
         ("melpa"        . "http://melpa.org/packages/")
         ("org"          . "http://orgmode.org/elpa/")))
 
-(defun require-package (package &optional min-version no-refresh)
-  "Install given PACKAGE, optionally requiring MIN-VERSION.
-  If NO-REFRESH is non-nil, the available package lists will not be
-  re-downloaded in order to locate PACKAGE."
-  (if (package-installed-p package min-version)
-      t
-    (if (or (assoc package package-archive-contents) no-refresh)
-        (package-install package)
-      (progn
-        (package-refresh-contents)
-        (require-package package min-version t)))))
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
 ;; Set the initial state to non-refreshed. This can also be set back
