d081330ddbcdd8a23eac5212886da886fc70d15c
Sean Farley <sean@farley.io>
2017-06-27 13:36:40 -0700

    imapnotify: switch back to mu4e

--- imapnotify.js
+++ imapnotify.js
@@ -11,6 +11,6 @@ exports.tls = true;
 exports.tlsOptions = { "rejectUnauthorized": false };
 exports.username = "sean@farley.io";
 exports.password = getStdout("/usr/bin/security find-internet-password -w -a sean@farley.io -s mail.farley.io");
-exports.onNewMail = "emacsclient -f ~/.emacs.d/server/server -qne \"(let (open-notmuch) (loop for buffer being the buffers do (if (s-starts-with? \\\"*notmuch-saved-search-\\\" (buffer-name buffer)) (setq open-notmuch t))) (when (not open-notmuch) (offlineimap)))\"";
+exports.onNewMail = "emacsclient -f ~/.emacs.d/server/server -qne \"(progn (mu4e 't) (mu4e-update-mail-and-index t))\""
 exports.onNewMailPost = "";
 exports.boxes = [ "INBOX" ];
