fbd91241810511371041f38db2b25300b95fe901
Sean Farley <sean@farley.io>
2018-05-15 19:35:23 -0700

    utils: add lfs helper class and refactor sidebar

--- sidebar.py
+++ sidebar.py
@@ -13,22 +13,47 @@ import CoreFoundation
 import LaunchServices
 
 
-def sidebar_list(*args, **opts):
-    """lists the current items in the finder sidebar"""
-    items = LaunchServices.LSSharedFileListCreate(
+class lsf(object):
+    """helper class to make SFLItem more pythonic"""
+    def __init__(self, ref):
+        self.ref = ref
+        self._name = None
+        self._path = None
+
+    @property
+    def name(self):
+        if self._name is None:
+            dname = LaunchServices.LSSharedFileListItemCopyDisplayName
+            # seems that this can be empty?
+            self._name = dname(self.ref) or "AirDrop"
+        return self._name
+
+    @property
+    def path(self):
+        if self._path is None:
+            presolve = LaunchServices.LSSharedFileListItemResolve
+            self._path = presolve(self.ref, 0, None, None)[1]
+        return self._path
+
+
+def all_items():
+    sitems = LaunchServices.LSSharedFileListCreate(
         CoreFoundation.kCFAllocatorDefault,
         LaunchServices.kLSSharedFileListFavoriteItems, None)
 
-    snapshot = LaunchServices.LSSharedFileListCopySnapshot(items, None)[0]
-    resolv = LaunchServices.LSSharedFileListItemResolve
+    snapshot = LaunchServices.LSSharedFileListCopySnapshot(sitems, None)[0]
 
-    for i in snapshot:
-        name = LaunchServices.LSSharedFileListItemCopyDisplayName(i)
-        path = ""
-        if name not in ("AirDrop", "All My Files", "iCloud"):
-            path = resolv(i, 0, None, None)[1]
+    return [lsf(i) for i in snapshot]
 
-        print("(%s, %s)" % (name, path))
+
+def sidebar_list(*args, **opts):
+    """lists the current items in the finder sidebar"""
+    items = all_items()
+
+    for i in items:
+        print("(%s, %s)" % (i.name, i.path))
+
+
 
 
 def main(*args, **opts):
