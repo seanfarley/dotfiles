3d368831e685b7e373e34b26d85efc5337610ea8
Sean Farley <sean@farley.io>
2018-11-08 13:34:09 -0800

    epass: add script to get password from emacs

---
+++ epass.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# if a username is supplied, then add :user to the auth-source call
+USERNAME=""
+[[ $2 != "" ]] && USERNAME=":user \"$2\""
+
+CMD="
+(funcall
+  (plist-get
+    (nth 0 (auth-source-search :host \"$1\"
+                               $USERNAME
+                               :max 1)) :secret))"
+
+pass=$(~/.edit.sh -n --eval "'$CMD'")
+
+# remove surrounding quotes
+pass="${pass%\"}"
+pass="${pass#\"}"
+
+if [[ "$pass" == "nil" ]]; then
+    exit 155
+fi
+
+echo "$pass"
