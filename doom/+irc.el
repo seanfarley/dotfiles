29d693ef4c17b8dee3e1e315ff17627f5ae89b00
Sean Farley <sean@farley.io>
2018-11-29 16:28:03 -0800

    doom: use bitwarden for irc

--- doom/+irc.el
+++ doom/+irc.el
@@ -8,20 +8,30 @@
   (setq circe-notifications-alert-icon
         (expand-file-name "irc.png" doom-private-dir)))
 
+(defmacro irc-password! (user)
+  "Returns a lambda that circe will evaluate for the password."
+  `(lambda (host)
+     (funcall (plist-get (car (auth-source-search
+                               :host host
+                               :user ,user))
+                         :secret))))
+
 (after! irc
   (set-irc-server! "freenode"
-                   '(:use-tls t
+                   `(:use-tls t
                      :nick "smf"
                      :user "smf/freenode"
+                     :pass ,(irc-password! "smf/freenode")
                      :port 6697
                      :host "smf.io"
                      :server-buffer-name "{network}:{host}:{port}"
                      :channels ("#mercurial" "#bitbucket")))
   
   (set-irc-server! "bitlbee"
-                   '(:use-tls t
+                   `(:use-tls t
                      :nick "smf"
                      :user "smf/bitlbee"
+                     :pass ,(irc-password! "smf/bitlbee")
                      :port 6697
                      :host "smf.io"
                      :server-buffer-name "{network}:{host}:{port}"
