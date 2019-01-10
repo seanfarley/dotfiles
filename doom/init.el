ad80dc3f2ee8a16cb99a2659775acef8f4d22f60
Sean Farley <sean@farley.io>
2019-01-10 15:54:13 -0800

    doom: turn on latexmk option

--- doom/init.el
+++ doom/init.el
@@ -103,7 +103,8 @@
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
-       latex             ; writing papers in Emacs has never been so fun
+       (latex            ; writing papers in Emacs has never been so fun
+        +latexmk)
        ;;ledger            ; an accounting system in Emacs
        ;; lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
