ffbd71b9caef25996d06bb5bbbd35a030975d63f
Sean Farley <sean@farley.io>
2019-02-07 21:20:42 -0800

    doom: add deadgrep

--- doom/packages.el
+++ doom/packages.el
@@ -2,6 +2,7 @@
 (package! banner-comment)
 (package! bitwarden :recipe (:fetcher github :repo "seanfarley/emacs-bitwarden"))
 (package! doom-modeline)
+(package! deadgrep)
 (package! emr)
 (package! forge)
 (package! goto-chg)
