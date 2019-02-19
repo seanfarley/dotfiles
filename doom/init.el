53b23245970ba1bd8a26069297d47e49b98c8921
Sean Farley <sean@farley.io>
2019-02-19 15:15:31 -0800

    doom: forge is on by default now

--- doom/init.el
+++ doom/init.el
@@ -67,8 +67,7 @@
        gist              ; interacting with github gists
        macos             ; MacOS-specific commands
        make              ; run make tasks from Emacs
-       (magit            ; a git porcelain for Emacs
-        +forge)          ; awesome git sugar
+       magit             ; a git porcelain for Emacs
        ;;password-store    ; password manager for nerds
        pdf               ; pdf enhancements
        prodigy           ; FIXME managing external services & code builders
