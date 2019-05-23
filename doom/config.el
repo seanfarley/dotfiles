7a8245b53dfe7040fbe60ebfcddd137dce1680a8
Sean Farley <sean@farley.io>
2019-05-23 16:27:44 -0700

    doom: set git-commit-summary-max-length to 80
    Fuck the haters.

--- doom/config.el
+++ doom/config.el
@@ -54,6 +54,13 @@
 (add-hook 'magit-process-find-password-functions
           'magit-process-password-auth-source)
 
+(defun smf/git-commit-conventions (&rest _)
+  "All opinions not my own are, by definition, bullshit."
+  (setq git-commit-summary-max-length 80))
+(add-hook 'git-commit-mode-hook #'smf/git-commit-conventions)
+(advice-add #'+vc|enforce-git-commit-conventions :after
+            #'smf/git-commit-conventions)
+
 ;; (setq doom-theme 'doom-snazzy)
 
 ;; nifty package for popping up a quick emacs frame
