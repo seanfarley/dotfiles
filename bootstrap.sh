d43ce4b79d21c2e228b4b62d61c63d5005881805
Sean Farley <sean@farley.io>
2019-06-04 15:37:01 -0700

    bootstrap: clean up old cruft
    Remove things I hardly use and, in some cases, no longer exist.

--- bootstrap.sh
+++ bootstrap.sh
@@ -47,35 +47,24 @@ mkdir -p ~/sandbox
 mkdir -p ~/.ipython
 mkdir -p ~/.ssh
 mkdir -p ~/.config/karabiner
-mkdir -p ~/.pastebin.d
 mkdir -p ~/.hammerspoon
-mkdir -p ~/.config/pass-git-helper
 
 [[ ! -d "$HOME/projects/doom-emacs" ]] && echo "doom not cloned yet" && exit 128
 
 ensure_link "aliases"
-ensure_link "aspell.en.prepl"
-ensure_link "aspell.en.pws"
 ensure_link "bash_prompt"
-ensure_link "ctags"
 ensure_link "dir_colors"
 ensure_link "../doom-emacs" ".emacs.d"
 ensure_link "doom" ".doom.d"
-ensure_link "edit.sh"
 ensure_link "exports"
 ensure_link "functions"
-ensure_link "gdbinit"
 ensure_link "gitconfig"
-ensure_link "globalrc"
 ensure_link "hgrc"
 ensure_link "hgignore"
 ensure_link "inputrc"
 ensure_link "ipython/profile_default"
 ensure_link "latexmkrc"
-ensure_link "lynx.lss"
-ensure_link "lynxrc"
 ensure_link "mbsyncrc"
-ensure_link "port-rdependents.py"
 ensure_link "profile"
 ensure_link "ssh/authorized_keys"
 ensure_link "ssh/config"
@@ -83,15 +72,9 @@ ensure_link "ssh/environment"
 ensure_link "flake8" ".config/flake8"
 ensure_link "profile" ".bashrc"
 ensure_link "hgignore" ".gitignore"
-ensure_link "pastebin.d/bpaste.net.conf"
-ensure_link "pastebinit.xml"
-ensure_link "imapnotify.js" ".config/imapnotify.js"
-ensure_link "vpnc-script" ".vpnc-script"
-ensure_link "pwclient"
 ensure_link "docker-bash"
 ensure_link "karabiner.json" ".config/karabiner/karabiner.json"
 ensure_link "hammerspoon.lua" ".hammerspoon/init.lua"
-ensure_link "git-pass-mapping.ini" ".config/pass-git-helper/git-pass-mapping.ini"
 ensure_link "gpg-agent.conf" ".gnupg/gpg-agent.conf"
 ensure_link "gpg.conf" ".gnupg/gpg.conf"
 ensure_link "xonshrc"
--- gdbinit
+++
@@ -1,5 +0,0 @@
-set disassembly-flavor intel
-set breakpoint pending on
-b PetscError
-set pagination 0
-set print pretty
