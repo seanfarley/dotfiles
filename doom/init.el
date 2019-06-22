c6cc589d4f31248f99390585198eebe287903735
Sean Farley <sean@farley.io>
2019-06-22 16:22:24 -0700

    doom: clean up whitespace

--- doom/init.el
+++ doom/init.el
@@ -54,8 +54,7 @@
        :emacs
        (dired            ; making dired pretty [functional]
         +ranger          ; bringing the goodness of ranger to dired
-        +icons           ; colorful icons for dired-mode
-        )
+        +icons)          ; colorful icons for dired-mode
        electric          ; smarter, keyword-based electric-indent
        vc                ; version-control and Emacs, sitting in a tree
 
