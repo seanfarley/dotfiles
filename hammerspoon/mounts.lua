8954b82ac3338dd7a9a9a29b5d3e40bc9cd64bb2
Sean Farley <sean@farley.io>
2019-06-21 18:40:34 -0700

    hammerspoon: add mounts module from jmartin

---
+++ hammerspoon/mounts.lua
@@ -0,0 +1,18 @@
+local alert = require "hs.alert"
+local volume = require "hs.fs.volume"
+
+local mod = {}
+
+function mod.unmountAll()
+  for volume_path, attributes in pairs(volume.allVolumes()) do
+    if not attributes.NSURLVolumeIsInternalKey and attributes.NSURLVolumeIsLocalKey and not attributes.NSURLVolumeLocalizedFormatDescriptionKey:lower():match('fuse') then
+      alert.show('Umounting: ' .. volume_path)
+      if not volume.eject(volume_path) then
+        alert.show('Failed to umount' .. volume_path)
+      end
+    end
+  end
+  alert.show('Unmounting done.')
+end
+
+return mod
