ec17f09cd759cb5ff1ac596417159558153541fe
Sean Farley <sean@farley.io>
2019-06-22 16:22:43 -0700

    doom: enable parinfer

--- doom/init.el
+++ doom/init.el
@@ -47,7 +47,7 @@
        ;;(format +onsave)  ; automated prettiness
        ;;lispy             ; vim for lisp, for people who dont like vim
        multiple-cursors  ; editing in many places at once
-       ;;parinfer          ; turn lisp into python, sort of
+       parinfer          ; turn lisp into python, sort of
        rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
 
