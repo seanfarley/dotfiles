308b995b3f716bf4ed5cb66078f003d34e99617b
Sean Farley <sean@farley.io>
2019-06-21 18:59:08 -0700

    hammerspoon: add windows module from jmartin
    Probably, most of this is unneeded but we'll remove more later as seen
    fit.

---
+++ hammerspoon/windows/extensions.lua
@@ -0,0 +1,184 @@
+-- initially from https://raw.githubusercontent.com/oskarols/dotfiles/0bd44443d00108e3c1a8d01520489e2d165f70ff/hammerspoon/extensions.lua
+
+local fnutils = require "hs.fnutils"
+local indexOf = fnutils.indexOf
+local filter = fnutils.filter
+local geometry = require "hs.geometry"
+local window = require "hs.window"
+local drawing = require "hs.drawing"
+local timer = require "hs.timer"
+local mouse = require "hs.mouse"
+local fs = require "hs.fs"
+local application = require "hs.application"
+local screen = require "hs.screen"
+local logger = hs.logger.new('windows.ext', 'debug')
+
+local mod = {}
+
+application.enableSpotlightForNameSearches(true)
+---------------------------------------------------------
+-- functools
+---------------------------------------------------------
+
+local function isFunction(a)
+  return type(a) == "function"
+end
+
+-- gets propery or method value
+-- on a table
+local function result(obj, property)
+  if not obj then return nil end
+
+  if isFunction(property) then
+    return property(obj)
+  elseif isFunction(obj[property]) then -- string
+    return obj[property](obj) -- <- this will be the source of bugs
+  else
+    return obj[property]
+  end
+end
+
+---------------------------------------------------------
+-- Coordinates, points, rects, frames, tables
+---------------------------------------------------------
+
+-- Fetch next index but cycle back when at the end
+--
+-- > getNextIndex({1,2,3}, 3)
+-- 1
+-- > getNextIndex({1}, 1)
+-- 1
+-- @return int
+local function getNextIndex(table, currentIndex)
+  local nextIndex = currentIndex + 1
+  if nextIndex > #table then
+    nextIndex = 1
+  end
+
+  return nextIndex
+end
+
+---------------------------------------------------------
+-- Mouse
+---------------------------------------------------------
+
+local mouseCircle = nil
+local mouseCircleTimer = nil
+
+function mod.mouseHighlight()
+  -- Delete an existing highlight if it exists
+  result(mouseCircle, "delete")
+  result(mouseCircleTimer, "stop")
+
+  -- Get the current co-ordinates of the mouse pointer
+  local mousepoint = mouse.getAbsolutePosition()
+
+  local circle = geometry.rect(mousepoint.x - 20, mousepoint.y - 20, 40, 40)
+
+  local fillColor = { red = 0.5, blue = 0.5, green = 0.5, alpha = 0.5 }
+
+  mouseCircle = drawing.circle(circle)
+  mouseCircle:setFillColor(fillColor)
+  mouseCircle:setStrokeWidth(0)
+  mouseCircle:show()
+
+  -- Set a timer to delete the circle after 3 seconds
+  mouseCircleTimer = timer.doAfter(0.2, function()
+    mouseCircle:delete()
+  end)
+end
+
+function mod.centerOnTitle(rect)
+  local point = geometry.rectMidPoint(rect)
+  point.y = rect.y + 5
+  mouse.setAbsolutePosition(point)
+end
+
+
+---------------------------------------------------------
+-- Application / window
+---------------------------------------------------------
+
+-- Returns the next successive window given a collection of windows
+-- and a current selected window
+--
+-- @param  windows  list of hs.window or applicationName
+-- @param  window   instance of hs.window
+-- @return hs.window
+local function getNextWindow(currentWindow)
+  local windows = currentWindow:application():allWindows()
+
+  windows = filter(windows, window.isStandard)
+  -- windows = filter(windows, window.isVisible)
+
+  -- need to sort by ID, since the default order of the window
+  -- isn't usable when we change the mainWindow
+  -- since mainWindow is always the first of the windows
+  -- hence we would always get the window succeeding mainWindow
+  table.sort(windows, function(w1, w2)
+    return w1:id() > w2:id()
+  end)
+
+  local lastIndex = indexOf(windows, currentWindow)
+
+  return windows[getNextIndex(windows, lastIndex)]
+end
+
+-- Needed to enable cycling of application windows
+local lastToggledAppName = ''
+
+function mod.launchOrCycleFocus(applicationName)
+  local function cleanupName(name)
+    return name:gsub('.app$', '')
+  end
+
+  return function()
+    local nextWindow = nil
+    local targetWindow
+    local focusedWindow = window.frontmostWindow()
+    if not focusedWindow then
+      application.launchOrFocus(applicationName:gsub('[0-9]+', ''))
+      return
+    end
+    local app = focusedWindow:application()
+
+    focusedWindow:focus()
+    local currentAppName
+    if app:path() then
+      currentAppName = cleanupName(focusedWindow and fs.displayName(app:path()))
+    end
+    lastToggledAppName = currentAppName
+
+    if applicationName == 'iTerm2' then
+      -- moving the cursor out the window, to preserve iTerm currently focused split
+      mouse.setAbsolutePosition(geometry.point(screen.mainScreen():fullFrame().w / 2, 5))
+    end
+
+    local appName = cleanupName(applicationName)
+    logger.df('last: %s, current: %s', currentAppName, appName)
+    if currentAppName == appName:gsub('[0-9]+', '') then
+      nextWindow = getNextWindow(focusedWindow)
+      nextWindow:becomeMain()
+    else
+      logger.df('launch or focus %s', app)
+      if not application.launchOrFocus(applicationName) then
+        application.launchOrFocus(applicationName:gsub('[0-9]+', ''))
+      end
+    end
+
+    if nextWindow then
+      targetWindow = nextWindow
+    else
+      targetWindow = window.focusedWindow()
+    end
+
+    if not targetWindow then
+      logger.df('failed finding a window for application: %s', applicationName)
+      return nil
+    end
+
+    mod.centerOnTitle(targetWindow:frame())
+  end
+end
+
+return mod
---
+++ hammerspoon/windows/init.lua
@@ -0,0 +1,214 @@
+local window = require "hs.window"
+local grid = require "hs.grid"
+local layout = require "hs.layout"
+local screen = require "hs.screen"
+local fnutils = require "hs.fnutils"
+local ext = require "windows/extensions"
+local logger = hs.logger.new('windows', 'debug')
+
+local mod = {}
+
+mod.launchOrCycleFocus = ext.launchOrCycleFocus
+
+-- grid/window settings
+grid.ui.textSize = 15
+grid.GRIDWIDTH = 10
+grid.GRIDHEIGHT = 4
+grid.MARGINX = 0
+grid.MARGINY = 0
+window.animationDuration = 0
+
+function mod.applyLayout(commonLayout, selectedLayout)
+  local function expandLayout(entry)
+    local scr
+    if entry.screenFn then
+      scr = entry.screenFn()
+    else
+      scr = screen.primaryScreen():name()
+    end
+    return { entry.name, nil, scr, entry.pos, nil, nil }
+  end
+
+  return function()
+    local completeLayout = fnutils.map(fnutils.concat(selectedLayout, commonLayout), expandLayout)
+    layout.apply(completeLayout)
+  end
+end
+
+local function toUnitRect(win)
+  local function round(value)
+    return math.ceil(value * 10) / 10
+  end
+
+  local unitRect = fnutils.map(win:screen():toUnitRect(win:frame()), round)
+  return { unitRect._x, unitRect._y, unitRect._w, unitRect._h }
+end
+
+local function isSamePos(currentPos, previousPos)
+  return
+    currentPos[0] == previousPos[0] and
+    currentPos[1] == previousPos[1] and
+    currentPos[2] == previousPos[2] and
+    currentPos[3] == previousPos[3]
+end
+
+local function inPostions(currentPos, positions)
+  for _, p in ipairs(positions) do
+    if isSamePos(currentPos, p) then
+      return true
+    end
+  end
+  return false
+end
+
+local function resolveTargetScreen(targetScreen)
+  if targetScreen == 'primary' then
+    return screen.primaryScreen()
+  end
+  return nil
+end
+
+local previousStates = {}
+
+function mod.moveWindowTo(pos, targetScreen)
+  local function buildKey(win)
+    return table.concat(pos, '\0') .. win:id()
+  end
+
+  return function()
+    local win = window:focusedWindow()
+    local winKey = buildKey(win)
+    local winPos = toUnitRect(win)
+    local previousState = previousStates[winKey]
+
+    if previousState and isSamePos(pos, winPos) then
+      win:move(previousState.pos, previousState.screen)
+      previousStates[winKey] = nil
+      logger.d('reverted to previousState')
+    else
+      previousStates[winKey] = { screen = win:screen(), pos = winPos }
+      win:move(pos, resolveTargetScreen(targetScreen))
+      logger.d('saved previousState')
+    end
+    ext.centerOnTitle(win:frame())
+  end
+end
+
+function mod.maximize()
+  window.focusedWindow():maximize()
+end
+
+local function isTableOfTables(t)
+  for _, v in ipairs(t) do
+    if type(v) ~= 'table' then
+      return false
+    end
+  end
+  return true
+end
+
+local function reverse(arr)
+  local i, j = 1, #arr
+  while i < j do
+    arr[i], arr[j] = arr[j], arr[i]
+    i = i + 1
+    j = j - 1
+  end
+end
+
+-- required for reseting the previous state.
+local cycleStates = {}
+
+-- cycles window size
+function mod.setPosition(positions, targetScreen, reversable)
+  if not isTableOfTables(positions) then
+    return mod.moveWindowTo(positions, targetScreen)
+  end
+
+  local nextPosFn
+
+  if reversable and screen.primaryScreen():name() == 'Color LCD' then
+    reverse(positions)
+  end
+
+  return function()
+    local win = window.frontmostWindow():focus()
+    local currentPos = toUnitRect(win)
+    local id = win:id()
+
+    if cycleStates[id] ~= positions or not inPostions(currentPos, positions) then
+      nextPosFn = fnutils.cycle(positions)
+    end
+
+    local nextPos = nextPosFn()
+
+    if isSamePos(nextPos, currentPos) then
+      logger.d('same postion, using next')
+      nextPos = nextPosFn()
+    end
+
+    cycleStates[id] = positions
+
+    win:move(nextPos, resolveTargetScreen(targetScreen))
+    ext.centerOnTitle(win:frame())
+  end
+end
+
+function mod.snapAll()
+  fnutils.each(window.visibleWindows(), grid.snap)
+end
+
+function mod.moveTo(pos)
+  return function()
+    window.focusedWindow():move(pos)
+    ext.centerOnTitle(pos)
+  end
+end
+
+function mod.previousScreen()
+  local win = window.focusedWindow()
+  win:moveToScreen(win:screen():previous())
+end
+
+function mod.nextScreen()
+  local win = window.focusedWindow()
+  win:moveToScreen(win:screen():next())
+end
+
+local function maximizeOrCycleScreen()
+  local win = window.focusedWindow()
+  if win:frame().w + 10 <= win:screen():frame().w then
+    win:maximize()
+    return win, nil
+  else
+    return win, win:screen()
+  end
+end
+
+function mod.cycleScreen()
+  local win, currentScreen = maximizeOrCycleScreen()
+  if currentScreen then
+    win:moveToScreen(currentScreen:next())
+  end
+  ext.centerOnTitle(win:frame())
+end
+
+function mod.cycleScreenBack()
+  local win, currentScreen = maximizeOrCycleScreen()
+  if currentScreen then
+    win:moveToScreen(currentScreen:previous())
+  end
+  ext.centerOnTitle(win:frame())
+end
+
+ function mod.alternateScreen()
+  local laptopScreen = 'Color LCD'
+  local extraScreen = 'SMS24A850'
+  if screen.find(extraScreen) then
+    return extraScreen
+  else
+    return laptopScreen
+  end
+end
+
+return mod
