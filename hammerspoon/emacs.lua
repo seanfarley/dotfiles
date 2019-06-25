51e6458bdd11e6efa29c0a3ccd04d9a7d8a2bb0b
Sean Farley <sean@farley.io>
2019-06-25 16:44:14 -0700

    hammerspoon: move emacs keybinding to emacs module

--- hammerspoon/emacs.lua
+++ hammerspoon/emacs.lua
@@ -25,6 +25,24 @@ local ec = "~/.ec"
 
 local appRequestingEmacs = nil
 
+-- map emacs keybindings in everything but the emacs app
+local esc = hs.hotkey.new({"ctrl"}, "g", function()
+      hs.eventtap.keyStroke({}, "escape")
+end)
+
+-- after much research, applicationWatcher is the best way to achieve what we
+-- want for ctrl-g with emacs
+local function applicationWatcher(appName, eventType, appObject)
+   if (eventType == hs.application.watcher.activated) then
+      if (appName == "Emacs") then
+         esc:disable()
+      else
+         esc:enable()
+      end
+   end
+end
+local appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()
 
 function backFromEmacs()
    if appRequestingEmacs == nil then
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -7,25 +7,6 @@ local reload = require 'utils/reload'
 -- make sure command-line utility is enabled and installed
 ipc.cliInstall()
 
--- map emacs keybindings in everything but the emacs app
-local esc = hs.hotkey.new({"ctrl"}, "g", function()
-      hs.eventtap.keyStroke({}, "escape")
-end)
-
--- after much research, applicationWatcher is the best way to achieve what we
--- want for ctrl-g with emacs
-local function applicationWatcher(appName, eventType, appObject)
-   if (eventType == hs.application.watcher.activated) then
-      if (appName == "Emacs") then
-         esc:disable()
-      else
-         esc:enable()
-      end
-   end
-end
-local appWatcher = hs.application.watcher.new(applicationWatcher)
-appWatcher:start()
-
 reload.init()
 keybindings.init()
 
