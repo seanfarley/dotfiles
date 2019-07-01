5890cc844dfdfc940ddfbef09e30e390be223b6e
Sean Farley <sean@farley.io>
2019-07-01 16:06:40 -0700

    hammerspoon: set log level to info

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,6 +4,9 @@ local emacs = require "emacs"
 local keybindings = require "keybindings"
 local reload = require 'utils/reload'
 
+-- 'info' is a bit too verbose
+hs.logger.setGlobalLogLevel('warning')
+
 -- make sure command-line utility is enabled and installed
 ipc.cliInstall()
 
