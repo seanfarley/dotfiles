022dff0c2d3733b464e9ca318daed5cfadd18d86
Sean Farley <sean@farley.io>
2019-06-30 23:06:37 -0700

    hammerspoon: add app filtering to mode
    This is just a copy-pasta from keybinder.

--- hammerspoon/mode.lua
+++ hammerspoon/mode.lua
@@ -4,12 +4,50 @@ local fnutils = require "hs.fnutils"
 local application = require "hs.application"
 
 local mod = {}
+local globalFilter = {}
 
 alert.defaultStyle['radius'] = 5
 alert.defaultStyle['textSize'] = 20
 
+local function initWatcher(appBindingMap)
+  local activated = {}
+  activated[application.watcher.activated] = true
+  activated[application.watcher.launched] = true
+  activated[application.watcher.launching] = true
+  activated[application.watcher.unhidden] = true
+
+  return
+    application.watcher.new(
+      function(appName, event, appObj)
+
+        -- re-enable global keybindings and disable filtered ones; only if we're
+        -- activating an app
+        if activated[event] ~= nil then
+          -- loop over the filtered apps
+          for app, filter in pairs(globalFilter) do
+            for _, key in ipairs(filter) do
+              -- re-enable all global keys (see below)
+              key:enable()
+            end
+          end
+
+          -- needs to be after re-enabling
+          for app, filter in pairs(globalFilter) do
+            for _, key in ipairs(filter) do
+              if app == appName then
+                -- disable the specified hotkeys
+                key:disable()
+              end
+            end
+          end
+        end
+
+      end
+    )
+end
+
 -- bindings { { key = 'string', fn = fn } }
-function mod.create(modifiers, key, name, bindings)
+function mod.create(modifiers, key, name, bindings, filter)
   local mode = hotkey.modal.new(modifiers, key)
 
   local function buildDesc(binding)
@@ -68,6 +106,19 @@ function mod.create(modifiers, key, name, bindings)
   mode:bind({'ctrl'}, 'g', exit)
   mode:bind({'ctrl'}, '[', exit)
   mode:bind(modifiers, key, exit)
+
+  -- 'filter' is a list of app to filter out, e.g. disable the global keybinding
+  -- for that particular app
+  if filter ~= nil then
+    for _, app in ipairs(filter) do
+      -- instantiate the list first
+      globalFilter[app] = globalFilter[app] or {}
+      table.insert(globalFilter[app], mode.k)
+    end
+  end
+
+
+  initWatcher(nil):start()
 end
 
 return mod
