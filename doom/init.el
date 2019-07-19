316ec97a86246466a486ff96369bede29cfa2716
Sean Farley <sean@farley.io>
2019-07-19 14:12:34 -0700

    doom: enable upload tool

--- doom/init.el
+++ doom/init.el
@@ -89,7 +89,7 @@
        ;;rgb               ; creating color strings
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
-       ;;upload            ; map local to remote projects via ssh/ftp
+       upload            ; map local to remote projects via ssh/ftp
        ;;wakatime
 
        :lang
