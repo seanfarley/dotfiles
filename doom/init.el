d539a1eaa4a82852c7d535a945997586a219f908
Sean Farley <sean@farley.io>
2019-07-19 14:12:17 -0700

    doom: update from upstream

--- doom/init.el
+++ doom/init.el
@@ -61,6 +61,7 @@
 
        :term
        eshell            ; a consistent, cross-platform shell (WIP)
+       ;;shell             ; a terminal REPL for Emacs
        ;;term              ; terminals in Emacs
        vterm             ; another terminals in Emacs
 
