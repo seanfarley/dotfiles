cafaa75039ea0f0a29ce53b75a829ef6bd4b41b2
Sean Farley <sean@farley.io>
2019-08-26 14:01:12 -0700

    doom: revamp persp behavior
    This adds helper methods to keep workspaces in most recently used order.

--- doom/+ui.el
+++ doom/+ui.el
@@ -36,3 +36,61 @@
       (setq-default ns-use-native-fullscreen nil))))
 
 (add-hook 'doom-init-ui-hook #'smf/init-mac-ui)
+
+(defun smf/auto-activate-venv (&rest _)
+  "Automatically activate virtualenv of same name, if one exists."
+  (pyvenv-deactivate)
+  (when-let ((workon-home (pyvenv-workon-home))
+             (window-name (safe-persp-name (get-current-persp))))
+    (if (not (file-directory-p workon-home))
+        (error "Can't find a workon home directory, set $WORKON_HOME"))
+    (dolist (name (f-directories workon-home))
+      (when (string= (file-name-nondirectory name) window-name)
+        (pyvenv-activate name)))))
+
+;; keep persp names in order of most recently used
+(after! persp-mode
+  (add-hook 'persp-before-switch-functions
+            #'(lambda (new-persp-name w-or-f)
+                (let ((cur-persp-name (safe-persp-name (get-current-persp))))
+                  (when (member cur-persp-name persp-names-cache)
+                    (setq persp-names-cache
+                          (cons cur-persp-name
+                                (delete cur-persp-name persp-names-cache)))))))
+
+  (add-hook 'persp-renamed-functions
+            #'(lambda (persp old-name new-name)
+                (setq persp-names-cache
+                      (cons new-name (delete old-name persp-names-cache)))))
+
+  (add-hook 'persp-before-kill-functions
+            #'(lambda (persp)
+                (setq persp-names-cache
+                      (delete (safe-persp-name persp) persp-names-cache))))
+
+  (add-hook 'persp-created-functions
+            #'(lambda (persp phash)
+                (when (and (eq phash *persp-hash*)
+                           (not (member (safe-persp-name persp)
+                                        persp-names-cache)))
+                  (setq persp-names-cache
+                        (cons (safe-persp-name persp) persp-names-cache)))))
+
+  ;; the above keeps the persp cache in MRU order
+  (defun smf/switch-to-last-workspace ()
+    "Function to switch to last used workspace.
+
+By 'last used,' I mean the last workspace that isn't an app. For
+simplicity, just test if the workspace begins with an asterik."
+    (when-let ((mru-list (cl-remove-if (lambda (it)
+                                         (or (string-prefix-p "*" it)
+                                             (string= "none" it)))
+                                       persp-names-cache)))
+      (persp-frame-switch (car mru-list))))
+
+  (defun smf/activate-emacs ()
+    "When activating emacs, only switch to last workspace when in an app."
+    (when (string-prefix-p "*" (+workspace-current-name))
+      (smf/switch-to-last-workspace)))
+
+  (advice-add #'persp-frame-switch :after #'smf/auto-activate-venv))
