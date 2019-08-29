ca1622bd331397d93cab805501eaf16719597104
Sean Farley <sean@farley.io>
2019-08-29 16:50:05 -0700

    doom: enable swift
    What hath I done?

--- doom/init.el
+++ doom/init.el
@@ -142,7 +142,7 @@
        ;;scala             ; java, but good
        sh                  ; she sells (ba|z|fi)sh shells on the C xor
        ;;solidity          ; do you need a blockchain? No.
-       ;;swift             ; who asked for emoji variables?
+       swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        web               ; the tubes
        ;;vala              ; GObjective-C
