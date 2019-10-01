81f096aef47631194230e83df8034f91dafa5104
Sean Farley <sean@farley.io>
2019-09-30 19:02:12 -0500

    doom: disable ein for now
    It's causing some build problems and I haven't used it in a while, so
    disabling for now.

--- doom/init.el
+++ doom/init.el
@@ -71,7 +71,7 @@
        ;;direnv
        docker
        editorconfig      ; let someone else argue about tabs vs spaces
-       ein               ; tame Jupyter notebooks with emacs
+       ;;ein               ; tame Jupyter notebooks with emacs
        eval              ; run code, run (also, repls)
        (flycheck         ; tasing you for every semicolon you forget
         +childframe)
