575c7277c70a77a9f80de47d126c813479e19186
Sean Farley <sean@farley.io>
2019-11-26 21:55:31 -0800

    doom: display eval runs as a tooltip overlay

--- doom/init.el
+++ doom/init.el
@@ -77,7 +77,7 @@
        docker
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
-       eval              ; run code, run (also, repls)
+       (eval +overlay)   ; run code, run (also, repls)
        (flycheck         ; tasing you for every semicolon you forget
         +childframe)
        flyspell          ; tasing you for misspelling mispelling
