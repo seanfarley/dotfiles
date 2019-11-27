0ac0032177c98ef959be853d16600ba1c3bf2b83
Sean Farley <sean@farley.io>
2019-11-26 21:52:26 -0800

    doom: enable scheme

--- doom/init.el
+++ doom/init.el
@@ -148,7 +148,7 @@
        ;;ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
-       ;;scheme            ; a fully conniving family of lisps
+       scheme            ; a fully conniving family of lisps
        sh                  ; she sells (ba|z|fi)sh shells on the C xor
        ;;solidity          ; do you need a blockchain? No.
        swift             ; who asked for emoji variables?
