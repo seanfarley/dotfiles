6a876b23f803d10c7c540351ab371081b2a3e4b2
Sean Farley <sean@farley.io>
2020-01-11 15:28:26 -0800

    doom: remove overlay because doom has one builtin

--- doom/init.el
+++ doom/init.el
@@ -95,7 +95,7 @@
        docker
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
-       (eval +overlay)   ; run code, run (also, repls)
+       eval              ; run code, run (also, repls)
        gist              ; interacting with github gists
        (lookup           ; helps you navigate your code and documentation
         +docsets         ; ...or in Dash docsets locally
