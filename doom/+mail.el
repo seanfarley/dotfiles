21b44fef5eae8a7ac2fba7d6ac0fe3f458e6c296
Sean Farley <sean@farley.io>
2020-02-19 17:20:25 -0800

    doom: whitespace cleanup in +mail

--- doom/+mail.el
+++ doom/+mail.el
@@ -58,13 +58,12 @@
    ;; mu4e-use-fancy-chars t
 
    mu4e-headers-fields
-   '( (:account       .   12)
-      (:human-date    .   12)
-      (:flags         .    6)
-      (:mailing-list  .   10)
-      (:from          .   22)
-      (:subject       .   nil))
-
+   '((:account       .   12)
+     (:human-date    .   12)
+     (:flags         .    6)
+     (:mailing-list  .   10)
+     (:from          .   22)
+     (:subject       .   nil))
 
    mu4e-bookmarks
    '(("maildir:/inbox AND flag:unread AND NOT flag:trashed" "All unread" ?u)
