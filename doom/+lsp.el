7fb94a6265f5941b2a1d32ba9189e2858983d4cd
Sean Farley <sean@farley.io>
2020-05-13 13:29:19 -0700

    doom: lsp was renamed to lsp-mode

--- doom/+lsp.el
+++ doom/+lsp.el
@@ -3,7 +3,7 @@
 (after! lsp-mode
   (add-hook 'lsp-after-open-hook 'lsp-enable-imenu))
 
-(after! lsp
+(after! lsp-mode
   (setq lsp-pyls-configuration-sources ["flake8"]
         lsp-pyls-plugins-pylint-enabled nil
         lsp-pyls-plugins-pycodestyle-enabled nil
