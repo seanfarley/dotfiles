6512494745fcd0e3bcb832507cf4517f3e59379b
Sean Farley <sean@farley.io>
2020-05-13 13:34:52 -0700

    doom: use my own repo for xonsh-mode instead of local path

--- doom/packages.el
+++ doom/packages.el
@@ -34,5 +34,5 @@
 (package! webpaste)
 (package! whole-line-or-region)
 (package! which-key-posframe)
-(package! xonsh-mode :recipe (:local-repo "~/projects/xonsh-mode"))
+(package! xonsh-mode :recipe (:host github :repo "seanfarley/xonsh-mode"))
 (package! ztree)
