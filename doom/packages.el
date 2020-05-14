e9612f1160485541a2840a8b58d0b81d7d8afe34
Sean Farley <sean@farley.io>
2020-05-14 12:23:31 -0700

    doom: remove unpin
    Let's try to be safer.

--- doom/packages.el
+++ doom/packages.el
@@ -1,5 +1,3 @@
-(unpin! t)
-
 (package! banner-comment)
 (package! bitwarden :recipe (:host github :repo "seanfarley/emacs-bitwarden"))
 (package! col-highlight :recipe (:host github :repo "emacsmirror/col-highlight"))
