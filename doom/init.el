ec080d8c61da0b8e59ac1ce1be76c91ede7b46b5
Sean Farley <sean@farley.io>
2020-05-14 12:25:11 -0700

    doom: remove twitter module

--- doom/init.el
+++ doom/init.el
@@ -189,7 +189,7 @@
        ;;calendar
        irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
-       twitter           ; twitter client https://twitter.com/vnought
+       ;; twitter           ; twitter client https://twitter.com/vnought
 
        :config
        ;;literate
