a096aa5bd9ea4adf828f156a024fea5f40b08479
Sean Farley <sean@farley.io>
2020-06-14 14:59:57 -0700

    doom: enable lsp for go

--- doom/init.el
+++ doom/init.el
@@ -133,7 +133,7 @@
        ;;faust             ; dsp, but you get to keep your soul
        ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
-       go                ; the hipster dialect
+       (go +lsp)         ; the hipster dialect
        ;;(haskell +intero) ; a language that's lazier than I am
        hy                ; readability of scheme w/ speed of python
        ;;idris             ;
