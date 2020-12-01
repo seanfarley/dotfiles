fa9de916664c373834e84abbf14d37fcf6b5f6f9
Sean Farley <sean@farley.io>
2020-12-01 15:31:23 -0600

    hammerspoon: load seal spoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -17,4 +17,24 @@ keybindings.init()
 slack.init()
 clipboard.init()
 
+hs.loadSpoon("Seal")
+
+spoon.Seal:loadPlugins({"apps", "screencapture", "calc", "useractions", "pasteboard"})
+
+spoon.Seal.plugins.apps.appSearchPaths = {
+   "/Applications",
+   "~/Applications",
+   "/System/Library/PreferencePanes",
+}
+
+spoon.Seal:refreshAllCommands()
+spoon.Seal:bindHotkeys({show = {{"cmd"}, "space"}})
+spoon.Seal.plugins.pasteboard.historySize = 4000
+
+spoon.Seal:start()
+
+-- since seal_apps starts the background spotlight work immediately; wait a bit
+-- and then force a restart since we modified the app search path
+hs.timer.doAfter(0.5, function () spoon.Seal.plugins.apps:restart() end)
+
 hs.alert.show("Config loaded")
