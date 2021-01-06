889c88f4f4b05687b3874fddad6f14517a31edf0
Sean Farley <sean@farley.io>
2021-01-05 21:07:38 -0600

    hammerspoon: remove slack bullshit

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,7 +3,6 @@ local ipc = require 'hs.ipc'
 local emacs = require "emacs"
 local keybindings = require "keybindings"
 local reload = require 'utils/reload'
-local slack = require 'slack'
 local clipboard = require 'clipboard'
 
 -- 'info' is a bit too verbose
@@ -14,7 +13,6 @@ ipc.cliInstall()
 
 reload.init()
 keybindings.init()
-slack.init()
 clipboard.init()
 
 hs.loadSpoon("Seal")
