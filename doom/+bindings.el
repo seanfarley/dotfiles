4356e368c12dcb3e6dafffce197408a51fd1566c
Sean Farley <sean@farley.io>
2021-03-19 14:02:58 -0500

    doom: even on linux use mac keybindings

--- doom/+bindings.el
+++ doom/+bindings.el
@@ -23,6 +23,41 @@
   ;; super common typo
   (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map))
 
+;; on linux, I still want mac keybindings ^_^
+(when IS-LINUX
+  (map! "s-`" #'other-frame  ; fix frame-switching
+        ;; fix OS window/frame navigation/manipulation keys
+        "s-w" #'delete-window
+        "s-W" #'delete-frame
+        "s-q" (if (daemonp) #'delete-frame #'save-buffers-kill-terminal)
+        "C-s-f" #'toggle-frame-fullscreen
+        ;; Restore somewhat common navigation
+        "s-l" #'goto-line
+        ;; Restore OS undo, save, copy, & paste keys (without cua-mode, because
+        ;; it imposes some other functionality and overhead we don't need)
+        "s-f" #'swiper
+        "s-z" #'undo
+        "s-Z" #'redo
+        "s-c" (if (featurep 'evil) #'evil-yank #'copy-region-as-kill)
+        "s-v" #'yank
+        "s-s" #'save-buffer
+        "s-x" #'kill-region
+        ;; Buffer-local font scaling
+        "s-+" #'doom/reset-font-size
+        "s-=" #'doom/increase-font-size
+        "s--" #'doom/decrease-font-size
+        ;; Conventional text-editing keys & motions
+        "s-a" #'mark-whole-buffer
+        "s-/" (cmd! (save-excursion (comment-line 1)))
+        :n "s-/" #'evilnc-comment-or-uncomment-lines
+        :v "s-/" #'evilnc-comment-operator
+        :gi  [s-backspace] #'doom/backward-kill-to-bol-and-indent
+        :gi  [s-left]      #'doom/backward-to-bol-or-indent
+        :gi  [s-right]     #'doom/forward-to-last-non-comment-or-eol
+        :gi  [M-backspace] #'backward-kill-word
+        :gi  [M-left]      #'backward-word
+        :gi  [M-right]     #'forward-word))
+
 (map!
  "s-k"                               #'kill-this-buffer
  "s-x"                               #'kill-region
