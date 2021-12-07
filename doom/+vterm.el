3c1be4673bf2a38b8bddde177b71734d3028a943
Sean Farley <sean@farley.io>
2021-12-07 11:05:56 -0600

    doom: seems that the face attribute is now fixed

--- doom/+vterm.el
+++ doom/+vterm.el
@@ -1,10 +1,6 @@
 (setq vterm-shell (executable-find "xonsh"))
 
-;; TODO upstream something to fix the autocomplete face color
-;; https://github.com/hlissner/emacs-doom-themes/issues/324#issuecomment-587164161
 (after! vterm
-  (set-face-attribute 'vterm-color-black nil
-                      :background "#9099AB") ;; a very bright grey
   (add-hook! vterm-mode #'goto-address-mode)
   (add-hook! vterm-mode (setq-local font-lock-keywords-only t)))
 
