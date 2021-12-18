4721663cf51039456bd68f5376551b25094ec218
Sean Farley <sean@farley.io>
2021-12-17 20:10:03 -0800

    chezmoi: add message for installing doom

--- run_once_05_install_doom.sh
+++ run_once_05_install_doom.sh
@@ -2,5 +2,9 @@
 
 [ -L "$HOME/.emacs.d" ] && exit
 
+echo "=============================================================================="
+echo "Installing doom emacs"
+echo "=============================================================================="
+
 git clone https://github.com/hlissner/doom-emacs.git "$HOME/projects/doom-emacs"
 ln -s "$HOME/projects/doom-emacs" "$HOME/.emacs.d"
