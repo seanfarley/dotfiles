6f3252a20592c017d51819618cf0d6ad6dc0a5b6
Sean Farley <sean@farley.io>
2021-12-21 18:24:04 -0600

    chezmoi: only clone doom if it doesn't exist already

--- run_once_05_install_doom.sh
+++ run_once_05_install_doom.sh
@@ -6,5 +6,5 @@ echo "==========================================================================
 echo "Installing doom emacs"
 echo "=============================================================================="
 
-git clone https://github.com/hlissner/doom-emacs.git "$HOME/projects/doom-emacs"
+[[ ! -d "$HOME/projects/doom-emacs" ]] && git clone https://github.com/hlissner/doom-emacs.git "$HOME/projects/doom-emacs"
 ln -s "$HOME/projects/doom-emacs" "$HOME/.emacs.d"
