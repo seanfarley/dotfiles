1f525bdb129dea7d2c3fe6e370832e22d0f44403
Sean Farley <sean@farley.io>
2021-12-23 17:02:35 -0600

    chezmoi: remove projects and sandbox directories
    Since we use soft links on euclid, we can't just blindly create the
    directories.

--- run_once_06_install_blesh.sh
+++ run_once_06_install_blesh.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# setup ~/sandbox first
+if [[ -d /hadron/euclid/home/sean && ! -L "$HOME/sandbox" ]]; then
+    ln -s /hadron/euclid/home/sean/sandbox "$HOME/sandbox"
+elif [[ ! -d "$HOME/sandbox" ]]; then
+    mkdir -p "$HOME/sandbox"
+fi
+
+
 [ -f "$HOME/.local/share/blesh/ble.sh" ] && exit
 
 echo "=============================================================================="
