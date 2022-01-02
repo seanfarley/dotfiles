52fa98a4fcd9bfcd3572e593ac66b9b1f60d0642
Sean Farley <sean@farley.io>
2022-01-02 16:36:06 -0600

    chezmoi: make sure to ssh-add for our private key

--- run_once_after_06_misc.sh
+++ run_once_after_06_misc.sh
@@ -7,6 +7,10 @@ elif [[ ! -d "$HOME/sandbox" ]]; then
     mkdir -p "$HOME/sandbox"
 fi
 
+if command -v fd-find >/dev/null 2>&1; then
+    # ensure the ssh key agent is open
+    ssh-add -K
+fi
 
 [ -f "$HOME/.local/share/blesh/ble.sh" ] && exit
 
