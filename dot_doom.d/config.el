1ac0a779393f68cacc40352c35d8dbfd7e53f40d
Sean Farley <sean@farley.io>
2022-01-02 13:05:45 -0600

    doom: add zsh to auto-mode-alist

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -52,6 +52,7 @@ Add it to a hook like so:
 (add-hook! ssh-config-mode #'display-line-numbers-mode)
 
 (add-to-list 'auto-mode-alist '("bash_.*\\'" . sh-mode))
+(add-to-list 'auto-mode-alist '("zsh.*\\'" . sh-mode))
 
 (after! magit
   ;; magit-todo ignore json files due to huge performance hit
