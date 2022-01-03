e02b25dc42052ad26b009ea33c78ff8644e00ac0
Sean Farley <sean@farley.io>
2022-01-02 19:06:09 -0600

    chezmoi: convert the git remote script to a template
    Previously, 'chezmoi diff' always would output the contents of the
    script since it would be applied every time. This allows our 'chezmoi
    diff' to be clean and only apply when it's needed.

--- run_after_80_git_remote.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd ~/projects/dotfiles || exit
-
-if [[ "$(git remote -v | tee /dev/null | grep origin | grep -c git@github.com:seanfarley/dotfiles.git)" == 0 ]]; then
-    git remote set-url origin "git@github.com:seanfarley/dotfiles.git"
-fi
