28a7ef36ab2605171eefe665cae4f9de62b8d0f7
Sean Farley <sean@farley.io>
2022-01-13 11:48:17 -0600

    zsh: experiment with different colors

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -375,6 +375,8 @@
       # Styling for up-to-date Git status.
       local       meta='%f'     # default foreground
       local      clean='%002F'  # green foreground
+      local      ahead='%176F'  # yellow foreground
+      local     behind='%081F'  # yellow foreground
       local   modified='%003F'  # yellow foreground
       local  untracked='%249F'  # blue foreground
       local conflicted='%001F'  # red foreground
@@ -429,10 +431,10 @@
     fi
 
     # ⇣42 if behind the remote.
-    (( VCS_STATUS_COMMITS_BEHIND )) && res+=" ${clean}⇣${VCS_STATUS_COMMITS_BEHIND}"
+    (( VCS_STATUS_COMMITS_BEHIND )) && res+=" ${behind}⇣${VCS_STATUS_COMMITS_BEHIND}"
     # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42.
     (( VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND )) && res+=" "
-    (( VCS_STATUS_COMMITS_AHEAD  )) && res+="${clean}⇡${VCS_STATUS_COMMITS_AHEAD}"
+    (( VCS_STATUS_COMMITS_AHEAD  )) && res+="${ahead}⇡${VCS_STATUS_COMMITS_AHEAD}"
     # # ⇠42 if behind the push remote.
     # (( VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" ${clean}⇠${VCS_STATUS_PUSH_COMMITS_BEHIND}"
     # (( VCS_STATUS_PUSH_COMMITS_AHEAD && !VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" "
