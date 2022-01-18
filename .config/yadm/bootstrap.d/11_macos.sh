86b5721cdb3c64da5d1382a4628be392f886cb51
Sean Farley <sean@farley.io>
2022-01-17 23:45:01 -0600

    macos: comment out older screen settings

--- .config/yadm/bootstrap.d/11_macos.sh
+++ .config/yadm/bootstrap.d/11_macos.sh
@@ -334,10 +334,10 @@ if [ "$system_type" = "Darwin" ] && [ $is_sudo = 1 ]; then
 
     # Enable subpixel font rendering on non-Apple LCDs
     # Reference: https://github.com/kevinSuttle/macOS-Defaults/issues/17#issuecomment-266633501
-    defaults write NSGlobalDomain AppleFontSmoothing -int 1
+    # defaults write NSGlobalDomain AppleFontSmoothing -int 1
 
     # Enable HiDPI display modes (requires restart)
-    sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
+    # sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
 
     ###############################################################################
     # Finder                                                                      #
