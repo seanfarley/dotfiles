33aba589d35efdcf9b14128217872accbeaa3d38
Sean Farley <sean@farley.io>
2022-01-19 18:40:45 -0600

    yadm: fallback to ~/.local/bin if yadm not found

--- .config/yadm/bootstrap.d/70_git_remote.sh
+++ .config/yadm/bootstrap.d/70_git_remote.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 echo "Updating the yadm repo origin URL"
-yadm remote set-url origin "git@github.com:seanfarley/dotfiles.git"
+
+yadm_bin="$(command -v yadm)"
+[ -z "$yadm_bin" ] && yadm_bin="$HOME/.local/bin/yadm"
+"$yadm_bin" remote set-url origin "git@github.com:seanfarley/dotfiles.git"
