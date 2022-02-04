9fa862a22a14091fbc33e4abf5fd17ce896d415c
Sean Farley <sean@farley.io>
2022-02-04 17:17:16 -0600

    macos: enable develop menu in safari

--- .config/yadm/bootstrap.d/11_macos.sh
+++ .config/yadm/bootstrap.d/11_macos.sh
@@ -642,9 +642,9 @@ if [ "$system_type" = "Darwin" ] && [ $is_sudo = 1 ]; then
     defaults write com.apple.Safari ProxiesInBookmarksBar "()"
 
     # Enable the Develop menu and the Web Inspector in Safari
-    # defaults write com.apple.Safari IncludeDevelopMenu -bool true
-    # defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-    # defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+    defaults write com.apple.Safari IncludeDevelopMenu -bool true
+    defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+    defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 
     # Add a context menu item for showing the Web Inspector in web views
     defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
