41f4777d7145eb9784d107dc60f1ab6f4494d960
Sean Farley <sean@farley.io>
2022-02-23 18:08:36 -0600

    doom: enable enchant spell checker

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -90,7 +90,7 @@
        :checkers
        (syntax           ; tasing you for every semicolon you forget
         +childframe)
-       spell             ; tasing you for misspelling mispelling
+       (spell +enchant)  ; tasing you for misspelling mispelling
        grammar           ; tasing grammar mistake every you make
 
        :tools
