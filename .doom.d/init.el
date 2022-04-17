ac2fad1f9f6635d5f77c8c37002aafb9bc22b7f2
Sean Farley <sean@farley.io>
2022-04-16 21:53:27 -0500

    doom: enable R via ess

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -140,7 +140,7 @@
        ;;elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
        ;;erlang            ; an elegant language for a more civilized age
-       ;;ess               ; emacs speaks statistics
+       ess               ; emacs speaks statistics
        ;;factor
        ;;faust             ; dsp, but you get to keep your soul
        ;;fsharp            ; ML stands for Microsoft's Language
