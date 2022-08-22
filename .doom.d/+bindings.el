851966cfe196bf292a9ffbf89fff61e9c58ce0d1
Sean Farley <sean@farley.io>
2022-08-22 13:51:13 -0500

    doom: enable lispy

--- .doom.d/+bindings.el
+++ .doom.d/+bindings.el
@@ -129,6 +129,12 @@
    "M-<left>"                      nil
    "C-M-d"                         nil))
 
+ ;; lispy
+ (:after lispy
+  (:map lispy-mode-map-lispy
+   "M-<right>"                     nil
+   "M-<left>"                      nil))
+
  ;; company
  (:after company
   (:map company-active-map
