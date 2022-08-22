bbb8655237aaa5a55cd0d97531604e9e1c03fc8f
Sean Farley <sean@farley.io>
2022-08-22 13:56:30 -0500

    doom: add +jq to rest

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -192,7 +192,7 @@
        ;; +lsp
        ;; +xp)
        ;;raku              ; the artist formerly known as perl6
-       rest              ; Emacs as a REST client
+       (rest +jq)        ; Emacs as a REST client
        rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
