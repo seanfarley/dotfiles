7098a479d411dd3c717d7ce2726d6dea1f65cb98
Sean Farley <sean@farley.io>
2022-08-22 12:53:44 -0500

    doom: enable vc-gutter pretty

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -52,7 +52,8 @@
        ;;tabs              ; an tab bar for Emacs
        (treemacs +lsp)     ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
-       vc-gutter         ; vcs diff in the fringe
+       (vc-gutter        ; vcs diff in the fringe
+        +pretty)         ; use thin bars instead of -,+
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
