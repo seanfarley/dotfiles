9d5b3240a76629b683d3176e0d26af89528d536e
Sean Farley <sean@farley.io>
2022-08-22 13:52:19 -0500

    doom: disable vertico +childframe
    Once I got over the visual placement change, vertico worked a bit better
    for things like searching and matching.

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -26,7 +26,7 @@
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        (vertico          ; a search engine for love and life
-        +childframe      ; modern popup ui
+        ;;+childframe      ; modern popup ui
         +icons)          ; pretty icons in buffer lists
 
        :ui
