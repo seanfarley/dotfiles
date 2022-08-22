94b9cb82d0c609316698fbc53f6c3e6bb39a59ca
Sean Farley <sean@farley.io>
2022-08-22 11:02:12 -0500

    yadm: update doom github url

--- .config/yadm/bootstrap.d/51_doom.sh
+++ .config/yadm/bootstrap.d/51_doom.sh
@@ -6,5 +6,5 @@ echo "==========================================================================
 echo "Installing doom emacs"
 echo "=============================================================================="
 
-[ ! -d "$HOME/projects/doom-emacs" ] && git clone https://github.com/hlissner/doom-emacs.git "$HOME/projects/doom-emacs"
+[ ! -d "$HOME/projects/doom-emacs" ] && git clone https://github.com/doomemacs/doomemacs.git "$HOME/projects/doom-emacs"
 ln -s "$HOME/projects/doom-emacs" "$HOME/.emacs.d"
