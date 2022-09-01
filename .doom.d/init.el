49d8ccfd1b798c08b0c7bb0215d8e32f64cd6e1d
Sean Farley <sean@farley.io>
2022-08-31 21:05:49 -0500

    doom: remove +cdlatex in favor of laas and snippets

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -163,7 +163,6 @@
        ;;kotlin            ; a better, slicker Java(Script)
        (latex            ; writing papers in Emacs has never been so fun
         +latexmk
-        +cdlatex
         +fold
         +lsp)
        ;;lean
