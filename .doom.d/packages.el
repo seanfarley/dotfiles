55a4adbd858ffce1f36e4fe2b96c8bacfcc57b21
Sean Farley <sean@farley.io>
2022-09-09 13:42:49 -0500

    doom: remove a whole bunch of unused packages
    Most of these packages were used only once or twice and then forgotten
    about. They all have great features but failed to substantially
    integrate into my own workflow.

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -1,4 +1,3 @@
-(package! ace-jump-zap)
 (package! benchmark-init)
 (package! biblio)
 (package! bitwarden :recipe '(:host github :repo "seanfarley/emacs-bitwarden"))
@@ -7,39 +6,26 @@
 (package! fancy-narrow)
 (package! flycheck-eldev)
 (package! flycheck-package)
-(package! focus)
-(package! goto-chg)
-(package! grip-mode)
 (package! highlight-sexp :recipe (:host github
                                   :repo "daimrod/highlight-sexp"))
-(package! lsp-focus :recipe '(:host github :repo "emacs-lsp/lsp-focus"))
 (package! lsp-origami)
 (package! lsp-treemacs)
 (package! message-view-patch)
 (package! nerd-font :recipe '(:host github :repo "twlz0ne/nerd-fonts.el"))
 (package! outline-minor-faces)
-(package! org-noter-pdftools)
-(package! org-recent-headings)
-(package! org-roam-bibtex)
 (package! org-sidebar)
-(package! org-web-tools)
 ;; (package! ox-report) ;; if I get back into contracting
 (package! package-lint)
 (package! page-break-lines)
 (package! prog-fill)
-(package! resize-window)
 (package! ssh-config-mode)
-(package! sx)
 (package! tramp-yadm :recipe (:local-repo "~/projects/tramp-yadm"
                               :build (:not compile)))
-(package! twauctex :recipe '(:host github :repo "jeeger/twauctex"))
 (package! unfill)
 (package! webpaste)
 (package! whole-line-or-region)
-(package! which-key-posframe)
 ;; too annoying for technical writing which uses a lot of passive voice
 (package! writegood-mode :disable t)
-(package! ztree)
 
 ;; I always forget this
 ;; (package! mypackage :recipe (:local-repo "packages/mypackage"
