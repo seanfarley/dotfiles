0f2ca8b4f7b5915c5601671390d77ee5db936712
Sean Farley <sean@farley.io>
2023-03-17 17:24:49 -0700

    doom: auto hide the compilation buffer if successful

--- .config/doom/+ui.el
+++ .config/doom/+ui.el
@@ -103,3 +103,38 @@ current window, as a ratio between 0 and 1.")
     (recenter line)))
 
 (global-set-key (kbd "C-l") 'g-recenter-window)
+
+;; from https://stackoverflow.com/a/73760499
+(defcustom auto-hide-compile-buffer-delay 0
+  "Time in seconds before auto hiding compile buffer."
+  :group 'compilation
+  :type 'number)
+
+(defun hide-compile-buffer-if-successful (buffer string)
+  (setq compilation-total-time (time-subtract nil compilation-start-time))
+  (setq time-str (concat " (Time: " (format-time-string "%s.%3N" compilation-total-time) "s)"))
+
+  (if
+      (with-current-buffer buffer
+        (setq warnings (eval compilation-num-warnings-found))
+        (setq warnings-str (concat " (Warnings: " (number-to-string warnings) ")"))
+        (setq errors (eval compilation-num-errors-found))
+
+        (if (eq errors 0) nil t))
+
+      ;;If Errors then
+      (message (concat "Compiled with Errors" warnings-str time-str))
+
+    ;;If Compiled Successfully or with Warnings then
+    (progn
+      (bury-buffer buffer)
+      (run-with-timer auto-hide-compile-buffer-delay nil 'delete-window (get-buffer-window buffer 'visible))
+      (message (concat "Compiled Successfully" warnings-str time-str)))))
+
+(make-variable-buffer-local 'compilation-start-time)
+
+(defun compilation-started (proc)
+  (setq compilation-start-time (current-time)))
+
+(add-hook 'compilation-start-hook 'compilation-started)
+(add-hook 'compilation-finish-functions 'hide-compile-buffer-if-successful)
