2a67ac0301ae28158ec93c284f659e2899248156
Sean Farley <sean@farley.io>
2023-05-16 11:55:58 -0700

    doom: add meson-mode

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -15,6 +15,7 @@
                                   :repo "daimrod/highlight-sexp"))
 (package! lsp-origami)
 (package! lsp-treemacs)
+(package! meson-mode)
 (package! message-view-patch)
 (package! mu4e-alert :disable t) ;; no longer needed after mu 1.10
 (package! nerd-font :recipe '(:host github :repo "twlz0ne/nerd-fonts.el"))
