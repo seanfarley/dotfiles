63f7bbec9a6087f097800700abda79749555de9f
Sean Farley <sean@farley.io>
2023-07-17 22:12:55 -0700

    macos: re-disable automatic corrections

--- .config/yadm/bootstrap.d/11_macos.sh
+++ .config/yadm/bootstrap.d/11_macos.sh
@@ -147,13 +147,13 @@ if [ "$system_type" = "Darwin" ] && [ $is_sudo = 1 ]; then
     # launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
 
     # Disable automatic capitalization as it’s annoying when typing code
-    # defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+    defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 
     # Disable smart dashes as they’re annoying when typing code
     # defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
     # Disable automatic period substitution as it’s annoying when typing code
-    # defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+    defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
 
     # Disable smart quotes as they’re annoying when typing code
     # defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
