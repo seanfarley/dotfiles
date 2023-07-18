16664cefa803d166bf836d9d5e92dfe6b90bd888
Sean Farley <sean@farley.io>
2023-07-17 21:16:08 -0700

    doom: enable common lisp

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -137,7 +137,7 @@
         +lsp
         +tree-sitter)
        (clojure +lsp)      ; java with a lisp
-       ;;common-lisp       ; if you've seen one lisp, you've seen them all
+       common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
