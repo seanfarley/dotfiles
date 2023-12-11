ea0609b8165672e25b3bc92fbfce101419215197
Sean Farley <sean@farley.io>
2023-12-11 13:30:28 -0800

    doom: enable eval +overlay
    This is the default now and is what lem does as well.

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -102,7 +102,7 @@
        docker
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
-       eval              ; run code, run (also, repls)
+       (eval +overlay)     ; run code, run (also, repls)
        gist              ; interacting with github gists
        (lookup           ; navigate your code and its documentation
         +docsets         ; ...or in Dash docsets locally
