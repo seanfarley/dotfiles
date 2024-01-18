22cd54fb33bed31a651f13674dce7e6354ec1708
Sean Farley <sean@farley.io>
2024-01-18 10:27:13 -0800

    doom: remove notes bookmark from mu4e
    It's long outdated and no longer needed.

--- .config/doom/+mail.el
+++ .config/doom/+mail.el
@@ -90,13 +90,6 @@
                :query "maildir:/inbox AND flag:unread AND NOT flag:trashed"
                :key ?u
                :favorite t)
-         (list :name "Notes"
-               :query (concat "to:sean.michael.farley+self "
-                              "AND NOT maildir:/phd "
-                              "AND NOT maildir:/posterity "
-                              "AND NOT flag:trashed "
-                              "AND NOT maildir:/Trash")
-               :key ?n)
          (list :name "PhD"
                :query (concat "((maildir:/phd AND flag:unread "
                               "  AND NOT flag:trashed) "
