1e3a064c89e4ccc99b86790dfd27ec98ae2ce134
Sean Farley <sean@farley.io>
2024-04-18 10:38:43 -0700

    doom: switch to diff-hl backend over vc-gutter

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -47,7 +47,9 @@
        ;;tabs              ; a tab bar for Emacs
        (treemacs +lsp)     ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
-       (vc-gutter +pretty) ; vcs diff in the fringe
+       (vc-gutter        ; vcs diff in the fringe
+        +diff-hl
+        +pretty)
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
