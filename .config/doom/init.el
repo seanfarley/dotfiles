8587c7f4132c5f0b4c26faced440910d0e4a198c
Sean Farley <sean@farley.io>
2024-04-18 10:35:29 -0700

    doom: turns out I don't really like snippets

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -64,7 +64,7 @@
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
-       snippets          ; my elves. They type so I don't have to
+       ;;snippets          ; my elves. They type so I don't have to
        word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
@@ -224,7 +224,7 @@
 
        :config
        ;;literate
-       (default +bindings +smartparens +snippets))
+       (default +bindings +smartparens))
 
 (when init-file-debug
   (require 'benchmark-init)
